10321f89ebba44bdb8b0cbdc93296ef180edf7ec
Luc <luc@worklib.io>
2026-01-22 11:12:21 +0100

    feat: descriptions and test, pre-commit added to project

--- dbt/models/staging/f1/stg_f1__circuits.sql
+++ dbt/models/staging/f1/stg_f1__circuits.sql
@@ -1,11 +1,11 @@
 SELECT
-    location,
-    country,
-    url AS circuit_url,
     CAST(circuitid AS integer) AS circuit_id,
     CAST(circuitref AS string) AS circuit_short_name,
     CAST(name AS string) AS circuit_long_name,
     CAST(lat AS float) AS latitude,
     CAST(lng AS float) AS longitude,
-    CAST(alt AS int) AS altitude
+    CAST(alt AS int) AS altitude,
+    location,
+    country,
+    url AS circuit_url
 FROM {{ source('f1', 'circuits') }}
